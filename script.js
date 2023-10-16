f35cc415ce644c025f8d9761b46150ca23c4dd18
Simon hovmand <simonhovmand@gmail.com>
2023-10-16 20:23:54 +0200

    Version 2.1

--- script.js
+++ script.js
@@ -18,9 +18,20 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
     document.getElementById("navbar").style.height = "70px";
-    document.getElementById("navbar-img").style.height = "60px";
+    document.getElementById("navbar-img").style.height = "50px";
   } else {
     document.getElementById("navbar").style.height = "100px";
-    document.getElementById("navbar-img").style.height = "90px";
+    document.getElementById("navbar-img").style.height = "70px";
   }
 }
+
+
+// typed js
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Lærer- og SFO vikar', 'Gymnast', 'Gymnastik Instruktør'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
